148536ff7a01e182c1a7d7615b5377d5aec6b400
jaitjacob <jai8.jacob@gmail.com>
2018-05-18 14:32:37 +1000

    learnt the document.write() method and JS arrays section. document.write() gives input directly to the parser.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var one = 1;
+function  daterepeat() {
+    console.log("date repeat function started")
+    var i;
+    for(i=0;i<10;i++){
+        var arr=["test","test1"];
+        arr[i]=prompt();
+        document.write("<h3>"+arr[i]+"</h3>");
+    }
+
+
+    console.log("date repeat function done");
+    }
